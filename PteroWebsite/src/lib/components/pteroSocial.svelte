22ef5bfca709b74dc53d2a1952421390f3332aee
Q <roussetquent1@gmail.com>
2021-10-26 10:27:34 -0400

    website improved

---
+++ PteroWebsite/src/lib/components/pteroSocial.svelte
@@ -0,0 +1,39 @@
+<script>
+  export let pteroSocialData = {};
+  const { HEADING, PTERO_SOCIAL } = pteroSocialData;
+</script>
+
+
+<section id="about-ptero" class="section grey-bgcolor">
+  <div class="container">
+    <h2 class="title text-center">{HEADING}</h2>
+    <div class="row section-body">
+      <div class="col-md-6">
+        <ul>
+          {#each PTERO_SOCIAL as list}
+            <li>{list}</li>
+          {/each}
+        </ul>
+      </div>
+      <div class="col-md-6">
+        <!-- <img src={} alt="" class="img-fluid" /> -->
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<style>
+  .about-title {
+    margin-top: 8%;
+    margin-bottom: 20px;
+  }
+
+  section ul li {
+    margin: 10px 0;
+  }
+
+  section ul {
+    padding-left: 23px;
+  }
+</style>
